a13568926b712bb6784506118c4cec754c383d9b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-14 09:41:51 -0600

    Next.js & Components & Pages

---
+++ components/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const user = ({ name, age }) => (
+    <div>
+        <h1>
+            { name }
+        </h1>
+        <p>
+            { age }
+        </p>
+    </div>
+);
+
+export default user;
