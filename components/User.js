c5c67a7399be4077503b2c363ac12e5d6aaf407f
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-14 09:44:57 -0600

    Styling our App in Next.js

--- components/User.js
+++ components/User.js
@@ -8,6 +8,14 @@ const user = ({ name, age }) => (
         <p>
             { age }
         </p>
+        <style jsx>{`
+            div {
+                border: 1px solid #eee;
+                box-shadow: 0 2px 3px #ccc;
+                padding: 20px;
+                text-align: center;
+            }
+        `}</style>
     </div>
 );
 
