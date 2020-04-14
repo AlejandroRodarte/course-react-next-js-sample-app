df33756fa392a352c300c769c91867cb2bed9f7f
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-14 10:01:44 -0600

    A Special Lifecycle Hook

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 
 import User from '../../components/User';
 
-const authIndexPage = () => (
+const authIndexPage = ({ appName }) => (
     <div>
         <h1>
-            The auth index page
+            The auth index page of { appName }
         </h1>
         <User name="Alejandro" age="28" />
     </div>
 );
+
+authIndexPage.getInitialProps = async () => {
+
+    const promise = new Promise((resolve, reject) => setTimeout(() => resolve({ appName: 'super-auth-app' }), 1000));
+    const data = await promise;
+
+    return data;
+};
 
 export default authIndexPage;
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
-const indexPage = () => (
-    <div>
-        <h1>
-            The main page
-        </h1>
-        <p>
-            Go to 
-            <Link href="/auth">
-                <a>
-                    Auth
-                </a>
-            </Link>
-        </p>
-        <button onClick={ () => Router.push('/auth') }>
-            Go to auth
-        </button>
-    </div>
-);
+class IndexPage extends Component {
 
-export default indexPage;
+    // populate from db before component even loads
+    // since this hook is called on the server
+    static async getInitialProps(context) {
+
+        // printed on the server
+        // pathname, query params, actual browser path, req and res objects, errors, etc
+        console.log(context);
+
+        const promise = new Promise((resolve, reject) => setTimeout(() => resolve({ appName: 'super-app' }), 1000));
+        const data = await promise;
+
+        return {
+            ...data,
+            ...context.query
+        };
+
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1>
+                    The main page of { this.props.appName } by { this.props.name }
+                </h1>
+                <p>
+                    Go to 
+                    <Link href="/auth">
+                        <a>
+                            Auth
+                        </a>
+                    </Link>
+                </p>
+                <button onClick={ () => Router.push('/auth') }>
+                    Go to auth
+                </button>
+            </div>
+        );
+
+    }
+   
+};
+
+export default IndexPage;
